b880a3c7e2f376a8ba82e70482ebf80046fba1d6
boram065 <boram4784@gmail.com>
2024-06-16 15:26:52 +0900

    Style: 스타일 수정

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,13 +2,20 @@ import { Icon } from "@iconify/react";
 
 import '../../styles/common/Style.css';
 import styles from '../../styles/main/main.module.css';
+import { useNavigate } from "react-router-dom";
 
 function Main() {
+    const nagivate = useNavigate();
+
+    const next = () => {
+        nagivate('/grade');
+    }
+
     return (
         <div className={styles['container']}>
             <img src="/images/click/Main.png" />
             <div className={styles['buttonContainer']}>
-                <button className={styles['button']}>
+                <button className={styles['button']} onClick={next}>
                     조언 매칭 테스트 시작하기
                     <Icon icon='ooui:arrow-next-ltr' />
                 </button>
